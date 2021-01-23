afcfd7f8743cee7f7f0fcf9d8ab92902a6cf02a5
barnadsv <barnadsv@gmail.com>
2021-01-23 16:16:21 -0300

    Importando e chamando proxyMiddleware corretamente

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
-import proxyMiddleware from 'http-proxy-middleware';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
@@ -19,7 +19,7 @@ const devProxy = {
 // Set up the proxy.
 if (dev && devProxy) {
 	Object.keys(devProxy).forEach(function(context) {
-	  	server.use(proxyMiddleware(context, devProxy[context]));
+	  	server.use(createProxyMiddleware(context, devProxy[context]));
 	});
 }
 
