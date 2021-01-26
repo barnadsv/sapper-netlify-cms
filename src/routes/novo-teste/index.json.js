83722ad9a683174fa35dd8ef025507ae2ec63510
barnadsv <barnadsv@gmail.com>
2021-01-26 12:02:57 -0300

    Salvando o arquivo index.json.js, que ainda estava com retorno estatico

--- src/routes/novo-teste/index.json.js
+++ src/routes/novo-teste/index.json.js
@@ -1,23 +1,19 @@
-// if (typeof fetch !== 'function') {
-//     global.fetch = require('node-fetch');
-// }
+if (typeof fetch !== 'function') {
+    global.fetch = require('node-fetch');
+}
 
-// const createTeste = (data) => {
-//     return fetch('/.netlify/functions/novo-teste', {
-//         body: JSON.stringify(data),
-//         method: 'POST'
-//     });
-// }
+const createTeste = (data) => {
+    return fetch('/.netlify/functions/novo-teste', {
+        body: JSON.stringify(data),
+        method: 'POST'
+    });
+}
 
-// export function post(req, res) {
-// 	res.writeHead(200, {
-// 		'Content-Type': 'application/json'
-//     });
-//     const data = req.body;
-// 	const novoTeste = createTeste(data);
-// 	res.end(JSON.stringify(novoTeste));
-// }
-
-return [
-    {"html": "Leonardo Barnabé", "name": "Leonardo", "slug":"leonardo"}
-];
+export function post(req, res) {
+	res.writeHead(200, {
+		'Content-Type': 'application/json'
+    });
+    const data = req.body;
+	const novoTeste = createTeste(data);
+	res.end(JSON.stringify(novoTeste));
+}
